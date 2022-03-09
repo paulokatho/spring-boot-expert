24d3a7af6681e69217bb6f01aacf9e69a023195c
paulo-pay <paulo.fernandes@payshopx.com.br>
2022-03-09 08:54:10 -0300

    feat: implementando mapeamento da classe Produto

--- src/main/java/br/com/katho/vendas/domain/entity/Produto.java
+++ src/main/java/br/com/katho/vendas/domain/entity/Produto.java
@@ -1,11 +1,20 @@
 package br.com.katho.vendas.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
