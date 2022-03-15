d5b8ef6cdc05de13c07e1651ca7a5af09dc08d2b
paulo-pay <paulo.fernandes@payshopx.com.br>
2022-03-15 08:31:29 -0300

    feat: criando api de busca por id

--- src/main/java/br/com/katho/vendas/rest/controller/ClienteController.java
+++ src/main/java/br/com/katho/vendas/rest/controller/ClienteController.java
@@ -1,26 +1,34 @@
 package br.com.katho.vendas.rest.controller;
 
+import br.com.katho.vendas.domain.entity.Cliente;
+import br.com.katho.vendas.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
-        return String.format("Hello %s ", nomeCliente);
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
     }
 
-    @RequestMapping(value = "/hello/{nome}",
-            method = RequestMethod.POST,
-            consumes = {"application/json", "application/xml"},
-            produces = {"application/json", "application/xml"},
-    )
+    @GetMapping("/{id}")
     @ResponseBody
-    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
-        return String.format("Hello %s ", nomeCliente);
-    }
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
 
+        if(cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
