6822d45233b82f4bf16859231c2055e9e8427aff
paulo-pay <paulo.fernandes@payshopx.com.br>
2022-03-26 11:49:50 -0300

    feat: implementando dependência do Dev Tools e controller advice com ExceptionHandlers para tratar erros na API

--- src/main/java/br/com/katho/vendas/rest/controller/PedidoController.java
+++ src/main/java/br/com/katho/vendas/rest/controller/PedidoController.java
@@ -1,8 +1,12 @@
 package br.com.katho.vendas.rest.controller;
 
+import br.com.katho.vendas.domain.entity.Pedido;
 import br.com.katho.vendas.domain.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.katho.vendas.rest.dto.PedidoDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -14,5 +18,11 @@ public class PedidoController {
         this.service = service;
     }
 
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Integer save( @RequestBody PedidoDTO dto ){
+        Pedido pedido = service.salvar(dto);
+        return pedido.getId();
+    }
 
 }
